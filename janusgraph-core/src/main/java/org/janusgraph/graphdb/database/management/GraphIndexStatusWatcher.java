5fddcfb08136a4e08ae4f9050754703111cf7aac
Pavel Ershov <owner.mad.epa@gmail.com>
2019-10-04 18:58:53 +0300

    Fix lookup for vertex with temporary id, when IDS_FLUSH = false
    Signed-off-by: Pavel Ershov <owner.mad.epa@gmail.com>

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/database/management/GraphIndexStatusWatcher.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/database/management/GraphIndexStatusWatcher.java
@@ -28,6 +28,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class GraphIndexStatusWatcher
         extends AbstractIndexStatusWatcher<GraphIndexStatusReport, GraphIndexStatusWatcher> {
@@ -60,6 +61,7 @@ public class GraphIndexStatusWatcher
         Timer t = new Timer(TimestampProviders.MILLI).start();
         boolean timedOut;
         while (true) {
+            Set<String> openInstances;
             JanusGraphManagement management = null;
             try {
                 management = g.openManagement();
@@ -72,6 +74,7 @@ public class GraphIndexStatusWatcher
                     else
                         converged.put(pk.toString(), s);
                 }
+                openInstances = management.getOpenInstances();
             } finally {
                 if (null != management)
                     management.rollback(); // Let an exception here propagate up the stack
@@ -88,8 +91,8 @@ public class GraphIndexStatusWatcher
             timedOut = null != timeout && 0 < t.elapsed().compareTo(timeout);
 
             if (timedOut) {
-                LOGGER.info("Timed out ({}) while waiting for index {} to converge on status(es) {}",
-                        timeout, graphIndexName, statuses);
+                LOGGER.warn("Timed out ({}) while waiting for index {} to converge on status(es) {}. Possible some of instances are dead {}",
+                        timeout, graphIndexName, statuses, openInstances);
                 return new GraphIndexStatusReport(false, graphIndexName, statuses, notConverged, converged, t.elapsed());
             }
             notConverged.clear();
